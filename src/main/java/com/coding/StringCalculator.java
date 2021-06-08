f7b0c6572c7cab74824bdd7d4713ee4fe732c3ed
vijayEE1997 <vijay1997dhakad@gmail.com>
2021-06-08 14:44:08 +0530

    merge to main

---
+++ src/main/java/com/coding/StringCalculator.java
@@ -0,0 +1,50 @@
+package com.coding;
+
+public class StringCalculator {
+	
+	static int count = 0;
+	public int getCalledCount() {
+		 ++count;
+		 return count;
+	}
+	
+	public int add(String numbers) {
+		
+		getCalledCount();
+		
+		if(numbers.equals(""))
+		{
+			return 0;
+		}
+		else
+		{
+			String delimiter = ",";
+			if(numbers.matches("//(.*)\n(.*)")) {
+				delimiter = Character.toString(numbers.charAt(2));
+				numbers = numbers.substring(4);
+			}
+			String[] number = numbers.split(delimiter +"|\n");
+
+			return utilSum(number);
+		}
+	}
+
+	private int utilSum(String[] number) {
+
+		String resultNegative="";
+		int result=0;
+		for(String num: number) {
+			if(Integer.parseInt(num)<0)
+				if(resultNegative=="")
+					resultNegative = num;
+				else
+					resultNegative +=(","+num);
+			if(Integer.parseInt(num)>1000)
+				continue;
+			result+=Integer.parseInt(num);
+		}
+		if(!resultNegative.equals(""))
+			throw new IllegalArgumentException("negatives not allowed : "+ resultNegative);
+		return result;
+	}
+}
---
+++ src/test/java/com/coding/StringCalculatorTest.java
@@ -0,0 +1,70 @@
+package com.coding;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class StringCalculatorTest {
+
+	StringCalculator sc = new StringCalculator();
+	
+	@Test
+	public void testEmptyString() {
+		assertEquals(0,sc.add(""));
+	}
+	
+	@Test
+	public void testOneNumber() {
+		assertEquals(1,sc.add("1"));
+	}
+	
+	@Test
+	public void testTwoNumber() {
+		assertEquals(3,sc.add("1,2"));
+	}
+	
+	@Test
+	public void testThreeNumber() {
+		assertEquals(8,sc.add("1,2,5"));
+	}
+
+	@Test
+	public void testNewLine() {
+		assertEquals(8,sc.add("1\n2,5"));
+	}
+	
+	@Test
+	public void testdifferentDelimiter() {
+		assertEquals(11,sc.add("//;\n2;9"));
+	}
+	
+	@Test
+	public void testSingleNegativesNotAllowed() {
+		try {
+			sc.add("-1,5,6");			
+		}
+		catch (IllegalArgumentException e) {
+			assertEquals(e.getMessage(),"negatives not allowed : -1");
+		}
+	}
+	@Test
+	public void testMultipleNegativesNotAllowed() {
+		try {
+			sc.add("-1,5,-6");			
+		}
+		catch (IllegalArgumentException e) {
+			assertEquals(e.getMessage(),"negatives not allowed : -1,-6");
+		}
+	}
+	
+	@Test
+	public void testNumberBiggerThenThousand() {
+			assertEquals(11,sc.add("1010,5,6"));
+	}
+	
+	@Test
+	public void testGetCount_2() {
+		assertEquals(8,sc.getCalledCount());
+	}
+
+}
