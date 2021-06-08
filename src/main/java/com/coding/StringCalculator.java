beba197a261807e1aee65004df33dbffe2bc5731
vijayEE1997 <vijay1997dhakad@gmail.com>
2021-06-08 14:41:26 +0530

    Refractor

--- src/main/java/com/coding/StringCalculator.java
+++ src/main/java/com/coding/StringCalculator.java
@@ -18,27 +18,33 @@ public class StringCalculator {
 		}
 		else
 		{
-			int result=0;
-			String resultNegative="";
 			String delimiter = ",";
 			if(numbers.matches("//(.*)\n(.*)")) {
 				delimiter = Character.toString(numbers.charAt(2));
 				numbers = numbers.substring(4);
 			}
 			String[] number = numbers.split(delimiter +"|\n");
-			for(String num: number) {
-				if(Integer.parseInt(num)<0)
-					if(resultNegative=="")
-						resultNegative = num;
-					else
-						resultNegative +=(","+num);
-				if(Integer.parseInt(num)>1000)
-					continue;
-				result+=Integer.parseInt(num);
-			}
-			if(!resultNegative.equals(""))
-				throw new IllegalArgumentException("negatives not allowed : "+ resultNegative);
-			return result;
+
+			return utilSum(number);
 		}
+	}
+
+	private int utilSum(String[] number) {
+
+		String resultNegative="";
+		int result=0;
+		for(String num: number) {
+			if(Integer.parseInt(num)<0)
+				if(resultNegative=="")
+					resultNegative = num;
+				else
+					resultNegative +=(","+num);
+			if(Integer.parseInt(num)>1000)
+				continue;
+			result+=Integer.parseInt(num);
+		}
+		if(!resultNegative.equals(""))
+			throw new IllegalArgumentException("negatives not allowed : "+ resultNegative);
+		return result;
 	}
 }
